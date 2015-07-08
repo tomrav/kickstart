3ac6f4bb12e161b564da37f95dee749272e21251
tomr <tomr@wix.com>
2015-07-08 19:55:49 +0300

    DomHelper is now a proper module.

--- dom-exercise/scripts/helpers/DomHelper.js
+++ dom-exercise/scripts/helpers/DomHelper.js
@@ -1,21 +1,22 @@
-var DomHelper = {
+var DomHelper = (function() {
+    return {
+        addClasses: function (element, classes) {
+            // first case for single class, second for array of classes
+            if (typeof classes === 'string') {
+                element.classList.add(classes);
+            } else if (Array.isArray(classes)) {
+                classes.forEach(function (value) {
+                    element.classList.add(value);
+                })
+            }
+        },
 
-    addClasses: function (element, classes) {
-        // first case for single class, second for array of classes
-        if (typeof classes === 'string') {
-            element.classList.add(classes);
-        } else if (Array.isArray(classes)) {
-            classes.forEach(function (value) {
-                element.classList.add(value);
-            })
+        insertAfter: function (newNode, referenceNode) {
+            referenceNode && referenceNode.parentNode && referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        },
+
+        removeElement: function (referenceNode) {
+            referenceNode && referenceNode.parentElement && referenceNode.parentElement.removeChild(referenceNode);
         }
-    },
-
-    insertAfter: function (newNode, referenceNode) {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-    },
-
-    removeElement: function (referenceNode) {
-        referenceNode && referenceNode.parentElement && referenceNode.parentElement.removeChild(referenceNode);
     }
-};
+}());
