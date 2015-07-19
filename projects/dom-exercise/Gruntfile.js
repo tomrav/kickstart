5fb3c34932668dff3eca60b61af65e0c25be4298
tomr <tomr@wix.com>
2015-07-20 01:15:09 +0300

    initial work on grunt

---
+++ projects/dom-exercise/Gruntfile.js
@@ -0,0 +1,101 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.config.init({
+        //watch: {
+        //    js: {
+        //        files: ['src/**/*.js'],
+        //        tasks: ['']
+        //    },
+        //    css: {
+        //        files: ['src/**/*.css'],
+        //        tasks: ['']
+        //    }
+        //},
+        clean: {
+            main: {
+                src: ['dist/**', 'build/**', '!dist/index.html']
+            },
+            temp: {
+                src: ['dist/*.concat.*']
+            }
+        },
+        cssmin: {
+            main: {
+                files: {
+                    'dist/style.min.css': ['dist/styles.concat.css']
+                }
+            }
+        },
+        processhtml: {
+            'dist/index.html': ['build/index.html']
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: './',
+                        src: ['**', '!scripts/views/**', '!node_modules/**', '!package.json'],
+                        dest: 'build/'
+                    }
+                ]
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            js: {
+                src: ['build/scripts/**'],
+                dest: 'dist/app.concat.js'
+            }
+        },
+        concat_css: {
+            main: {
+                src: ['build/styles/helpers.css', 'build/styles/colors.css', 'build/styles/typography.css', 'build/styles/layout.css'],
+                dest: 'dist/styles.concat.css'
+            }
+        },
+        uglify: {
+            main: {
+                files: {
+                    'dist/app.min.js': ['dist/app.concat.js']
+                }
+            }
+        },
+        handlebars: {
+            main: {
+                options: {
+                    namespace: 'Templates',
+                    processName: function(filePath) {
+                        return filePath.replace(/^scripts\/views\//, '').replace(/\.hbs$/, '');
+                    }
+                },
+                files: {
+                    'build/scripts/templates.jst': ['scripts/views/**/*.hbs']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-concat-css');
+
+    grunt.registerTask('default', ['clean:main', 'handlebars', 'copy']);
+    grunt.registerTask('dist', ['concat', 'concat_css', 'processhtml','uglify', 'cssmin', 'clean:temp']);
+
+    //grunt.registerTask('check', ['']);
+    //grunt.registerTask('build', ['clean', 'copy', 'processhtml']);
+    //grunt.registerTask('dev', ['connect:dev']);
+    //grunt.registerTask('prod', ['default', 'connect:prod']);
+
+};
